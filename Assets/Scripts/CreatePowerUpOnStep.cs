d64219cbce2a3033663b4f68f041ed378d40db2f
Azael <yohan-roux@hotmail.fr>
2018-12-02 13:28:56 +0100

    Add Sound

---
+++ Assets/Scripts/CreatePowerUpOnStep.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePowerUpOnStep : MonoBehaviour
+{
+
+	private bool isAlreadyStep = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.name == "MasterCultist" && !isAlreadyStep)
+		{
+			Debug.Log("Power Up");
+			// Change Color
+			Renderer r = this.GetComponent<Renderer>();
+			r.material.SetColor("_test", Color.white);
+			isAlreadyStep = true;
+		}
+	}
+
+	private void Update()
+	{
+		
+	}
+}
