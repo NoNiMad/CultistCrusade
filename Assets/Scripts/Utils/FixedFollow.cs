449d6872a78ccbadf375cb9489854823b2fc853a
NoNiMad <nonimad@free.fr>
2018-12-02 16:06:04 +0100

    Camera follows the player

---
+++ Assets/Scripts/Utils/FixedFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedFollow : MonoBehaviour {
+    public Transform toFollow;
+
+    Vector3 offset;
+
+	void Start () {
+        offset = transform.position - toFollow.position;
+	}
+	
+	void LateUpdate () {
+        transform.position = offset + toFollow.position;
+	}
+}
