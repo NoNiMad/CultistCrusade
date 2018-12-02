445d930691b2003d4e80a2de4974f8ca8a085dd5
Azael <yohan-roux@hotmail.fr>
2018-12-02 20:28:26 +0100

    add UI interface and Favor minimal system

---
+++ Assets/Scripts/Favours.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Favours : MonoBehaviour
+{
+	public Text text;
+	public int maxFavour;
+	public int currentFavour { get; private set; }
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentFavour = 0;
+		text = GetComponent<Text>();
+	}
+
+	public void AddOrRemoveFavour(int favor)
+	{
+		currentFavour += favor;
+		if (currentFavour < 0) currentFavour = 0;
+		if (currentFavour > maxFavour) currentFavour = maxFavour;
+		text.text = currentFavour.ToString();
+	}
+}
