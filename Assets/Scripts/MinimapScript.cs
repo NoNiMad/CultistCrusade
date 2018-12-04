f4f26e69a7cec7c1bf2a8739734f8279c9132c23
Ourten <ourtencoop@gmail.com>
2018-12-04 01:10:56 +0100

    Add Minimap

---
+++ Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 10;
+
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y + distance, target.position.z);
+    }
+}
