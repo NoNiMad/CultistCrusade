fef3169dd4f12c98fc0070dda33153c8bff253ff
Azael <yohan-roux@hotmail.fr>
2018-12-04 03:35:44 +0100

    add portal & end game

---
+++ Assets/Scripts/PortalWin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalWin : MonoBehaviour {
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.GetComponent<PlayerController>())
+			SceneManager.LoadScene("CloseScene");
+	}
+}
